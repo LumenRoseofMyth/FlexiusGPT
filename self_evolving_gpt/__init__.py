b89307220d66aa3bdf971a20ca61f82ac0a521e8
LumenRoseofMyth <lumenroseofmyth@gmail.com>
2025-06-21 05:13:27 +0800

    feat: add patch summarization agent

---
+++ self_evolving_gpt/__init__.py
@@ -0,0 +1,9 @@
+from .gpt_client import GPTClient
+from .prompt_builder import PromptBuilder
+from .agents import PatchSummarizationAgent
+
+__all__ = [
+    "GPTClient",
+    "PromptBuilder",
+    "PatchSummarizationAgent",
+]
